ef5d91a0be768382c73e29c7e2fe9e56e7b8e373
Jaehee Jung <wogml414@gmail.com>
2023-09-12 22:27:23 +0900

    sorting_33333

--- sorting.c
+++ sorting.c
@@ -6,7 +6,7 @@
 /*   By: jaehejun <jaehejun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/12 20:48:33 by jaehejun          #+#    #+#             */
-/*   Updated: 2023/09/12 22:22:21 by jaehejun         ###   ########.fr       */
+/*   Updated: 2023/09/12 22:24:27 by jaehejun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,30 +51,12 @@ void	sorting_three(t_all *all)
 	t_node	*temp;
 	
 	temp = all->stack_a->top;
-	//021->102, 120->012
 	if (temp->num < temp->next->num && temp->next->num > temp->next->next->num)
-	{
 		rra(all->stack_a);
-	}
-	//201->012, 210->102
 	if (temp->num > temp->next->num && temp->num > temp->next->next->num)
-	{
 		ra(all->stack_a);
-	}
-	//012
 	temp = all->stack_a->top;
 	if (temp->num < temp->next->num && temp->next->num < temp->next->next->num)
-	{
 		return ;
-	}
-	//213->123
-	//if (temp->num > temp->next->num && temp->num < temp->next->next->num)
 	sa(all->stack_a);
-	//231
-	//else if (temp->num < temp->next->num && temp->num > temp->next->next->num)
-	//	rra(all->stack_a);
-	////312
-	//else if (temp->num > temp->next->num && temp->num > temp->next->next->num)
-	//	ra(all->stack_a);
-	//123
 }
